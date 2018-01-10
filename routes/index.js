edd66e0398d5175eecb1b8783adcd2e94d4b6bf2
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2018-01-10 13:08:58 +0100

    JSON by nan

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'pasaia',
+    database : 'errolda'
 });
+
+connection.connect();
+
+router.get('/nan/:id', function(req, res, next) {
+    var nan = req.params.id;
+
+    var sql = "SELECT * from biztanleak WHERE `DNI CIF` LIKE '%" + nan + "%'";
+    connection.query(sql, function(err, results, fields) {
+        if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            return res.json({ errors: ["Could not retrieve photo"] });
+        }
+
+        // No results returned mean the object is not found
+        if (results.length === 0) {
+            // We are able to set the HTTP status code on the res object
+            res.statusCode = 404;
+            return res.json({ errors: ["Ez da topatu"] });
+        }
+
+        res.json(results);
+        connection.end();
+    });
+});
+
+
 
 module.exports = router;
